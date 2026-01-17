8c856ba8c8b9955c7edfe10abcdf28982d7c396b
aaron1458 <aaron.bartolome.code@gmail.com>
2026-01-17 17:59:36 +0800

    add tailwind updates

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#10b981",
+          dark: "#065f46",
+          DEFAULT: "#047857",
+        },
+        secondary: {
+          light: "#6b7280",
+          dark: "#1f2937",
+        },
+        success: "#10b981",
+        warning: "#f59e0b",
+        danger: "#ef4444",
+        info: "#3b82f6",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
