070d0767063735ea95097dcf08dbf88f3bf1d539
aaron1458 <aaron.bartolome.code@gmail.com>
2026-01-20 11:26:35 +0800

    add Card component and update dashboard layout with dynamic data for Passengers, Drivers, Rides, and Admins

---
+++ app/components/ui/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { LucideIcon } from "lucide-vue-next";
+defineProps<{
+  id: string;
+  label: string;
+  value: number;
+  icon?: LucideIcon;
+  href: string;
+}>();
+</script>
+
+<template>
+  <div class="bg-secondary-dark rounded-lg shadow-lg p-6">
+    <h5 :for="id" class="block text-lg font-semibold text-secondary-light mb-2">
+      {{ label }}
+    </h5>
+    <div class="text-4xl font-bold text-white mb-6 text-center">
+      {{ value }}
+    </div>
+    <NuxtLink
+      :to="href"
+      class="w-full btn-primary flex items-center gap-2 justify-center"
+    >
+      <component :is="icon" v-if="icon" :size="20" />
+      <span>View {{ label }}</span>
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,5 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Users, IdCard, CarTaxiFront, UserStar } from "lucide-vue-next";
+import { ref } from "vue";
+
+const passengers = ref(1);
+const drivers = ref(3);
+const rides = ref(5);
+const admins = ref(2);
+</script>
 
 <template>
-  <div>Dashboard</div>
+  <div class="flex flex-col gap-8">
+    <h2 class="text-white text-4xl">Dashboard</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <uiCard
+        id="passengers"
+        label="Passengers"
+        :value="passengers"
+        :icon="Users"
+        href="/passengers"
+      />
+      <uiCard
+        id="drivers"
+        label="Drivers"
+        :value="drivers"
+        :icon="IdCard"
+        href="/drivers"
+      />
+      <uiCard
+        id="rides"
+        label="Rides"
+        :value="rides"
+        :icon="CarTaxiFront"
+        href="/rides"
+      />
+      <uiCard
+        id="admins"
+        label="Admins"
+        :value="admins"
+        :icon="UserStar"
+        href="/admins"
+      />
+    </div>
+  </div>
 </template>
